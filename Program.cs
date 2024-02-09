5863facd35f22d00b362efb57b9b9af8acbbb795
Jenyffer Prochno <jlprochno@gmail.com>
2024-02-09 09:47:11 -0300

    Atividade desenvolvida para fixação de conhecimento no curso de .Net promovido pela PUC-PR.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace avarageNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number;
+        int sum = 0;
+        int counter = 0;
+        double avarage = 0;
+
+        Console.WriteLine("Olá, por gentileza, digite alguns números. E quando desejar encerrar e calcular a média, digite -1.");
+
+    do{
+        Console.WriteLine("Digite um número: ");
+        number = Convert.ToInt32(Console.ReadLine());            
+            if(number != -1){
+            sum += number;                 
+            counter ++;
+        }            
+    }while(number != -1);
+
+    avarage = sum / counter;
+
+    Console.WriteLine($"Você digitou {counter} números e a média deles é de {avarage}.");
+    }
+
+}
+
